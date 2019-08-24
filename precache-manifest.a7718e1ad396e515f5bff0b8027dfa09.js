39fce46ec46a1e1d67450bf07f582f63e2fc13d3
cgrossi <cgrossi@gmail.com>
2019-08-23 18:08:00 -0600

    Init commit

---
+++ precache-manifest.a7718e1ad396e515f5bff0b8027dfa09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe170526d4892abad7008d8802cf00a",
+    "url": "/url-shorten/index.html"
+  },
+  {
+    "revision": "c174a372e35f74600c07",
+    "url": "/url-shorten/static/css/main.9f6dd64b.chunk.css"
+  },
+  {
+    "revision": "72bfd67914741318a562",
+    "url": "/url-shorten/static/js/2.e382f7a8.chunk.js"
+  },
+  {
+    "revision": "c174a372e35f74600c07",
+    "url": "/url-shorten/static/js/main.56240248.chunk.js"
+  },
+  {
+    "revision": "5768c395483da1f80340",
+    "url": "/url-shorten/static/js/runtime~main.586d18f0.js"
+  },
+  {
+    "revision": "c50912609e5ac3e362433ee11296ed5a",
+    "url": "/url-shorten/static/media/clip.c5091260.svg"
+  }
+]);
